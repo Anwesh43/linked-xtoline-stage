d1277095605f789870083ac1f2692fcb160a2512
Anwesh43 <anweshthecool0@gmail.com>
2018-09-08 15:06:58 +0530

    adding LinkedXToLineStage class

---
+++ LinkedXToLineStage.ts
@@ -0,0 +1,30 @@
+const w : number = window.innerWidth, h : number = window.innerHeight, nodes : number = 5
+class LinkedXToLineStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = '#212121'
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage = new LinkedXToLineStage()
+        stage.render()
+        stage.handleTap()
+    }
+}
